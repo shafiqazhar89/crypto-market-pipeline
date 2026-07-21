6caa7e8a7dca86e524193ece7abf390f76e4b667
muhammad.shafiq.azhar <shafiqazhar89@gmail.com>
2026-07-21 17:42:31 +0800

    feat: dlt pipeline for CoinGecko coins/markets into raw schema

---
+++ dlt/rest_api_pipeline.py
@@ -0,0 +1,53 @@
+import dlt
+from dlt.sources.rest_api import (
+    RESTAPIConfig,
+    rest_api_resources,
+)
+
+
+@dlt.source
+def coingecko_source(api_key=dlt.secrets.value):
+    config: RESTAPIConfig = {
+        "client": {
+            "base_url": "https://api.coingecko.com/api/v3/",
+            "auth": {
+                "type": "api_key",
+                "name": "x-cg-demo-api-key",
+                "api_key": api_key,
+                "location": "header",
+            },
+        },
+        "resources": [
+            {
+                "name": "coins_markets", 
+                "endpoint": {
+                    "path": "coins/markets",
+                     # Query parameters for the endpoint
+                     "params": {
+                         "vs_currency": "usd",
+                         "per_page": 250,
+                         "page": 1
+                     }
+                    
+                    }
+            }
+        ],
+    }
+    yield from rest_api_resources(config)
+
+
+def load_coingecko() -> None:
+    pipeline = dlt.pipeline(
+        pipeline_name="crypto_market_pipeline",
+        destination=dlt.destinations.duckdb(
+            credentials="/home/shafiq/crypto-market-pipeline/.duckdb/dev.duckdb"
+        ),
+        dataset_name="raw",
+    )
+    load_info = pipeline.run(coingecko_source())
+    print(load_info)
+
+
+
+if __name__ == "__main__":
+    load_coingecko()
